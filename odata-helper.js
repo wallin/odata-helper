849d3e26f58340b004a0ec0beb4c1c922a345135
Sebastian Wallin <sebastian.wallin@gmail.com>
2011-07-04 15:52:08 +0200

    First commit

---
+++ odata-helper.js
@@ -0,0 +1,167 @@
+//
+// Simple OData query string builder
+//
+
+(function (window) {
+  // Builderfunctions for different types
+  var paramSingle = function (name) {
+    return function (val) {
+      if (typeof(val) === 'number' || typeof(val) === ('string')) {
+        this._params[name] = val;
+      }
+      return this;
+    };
+  };
+
+  var paramMulti = function (name) {
+    return function () {
+      var str = '';
+      for (var i = 0, len = arguments.length; i < len; i++) {
+        if (i !== 0) {
+          str += ',';
+        }
+        str += arguments[i];
+      }
+      if (str.length > 0) {
+        this._params[name] = str;
+      }
+      return this;
+    };
+  };
+
+  var paramSort = function (name) {
+    return function () {
+      if (this._params &&
+          this._params.orderby) {
+        this._params.orderby += ' ' + name;
+      }
+      return this;
+    };
+  };
+
+  // Define valid parameters
+  var queryParams = {
+    expand: paramMulti,
+    orderby: paramMulti,
+    select: paramMulti,
+
+    asc: paramSort,
+    desc: paramSort,
+
+    callback: paramSingle,
+    filter: paramSingle,
+    inlinecount: paramSingle,
+    top: paramSingle,
+    skip: paramSingle
+  };
+
+  // Generate prototype functions as specified in queryParams
+  var odataQueryFuncs = function () {};
+  for (var j in queryParams) {
+    if (queryParams.hasOwnProperty(j)) {
+      var fn = queryParams[j];
+      odataQueryFuncs[j] = fn(j);
+    }
+  }
+
+  // Generates URL-string from odataService object
+  var toString = function () {
+    var segments = [this._root];
+    var qry = [];
+    if (this._params) {
+      for (var i in this._params) {
+        if (this._params.hasOwnProperty(i) &&
+            (i in queryParams)) {
+          var item = this._params[i];
+          qry.push('$' + i + '=' + item);
+        }
+      }
+      if (this._format) {
+        qry.push('$format=' + this._format);
+      }
+      this._params = {};
+    }
+    if (this._default) {
+      var def = this._default;
+      if (this._id) {
+        def += '(' + this._id + ')';
+        this._id = null;
+      }
+      segments.push(def);
+    }
+    if (this._resource && this._resource.length > 0) {
+      segments = segments.concat(this._resource);
+      this._resource = [];
+    }
+    if (qry.length > 0) {
+      segments.push('?' + qry.join('&'));
+    }
+    return segments.join('/');
+  };
+
+
+  // odataService object helper methods
+  var setId = function (id) {
+    if (id || id === 0 && this._default) {
+      this._id = id;
+    }
+    return this;
+  };
+
+  var addResource = function (resource, id) {
+    if (id || id === 0) {
+      resource += '(' + id + ')';
+    }
+    this._resource.push(resource);
+    return this;
+  };
+
+  // Describe an OData service URI
+  var odataService = function (serviceURL, format) {
+    var proto = {
+      _root: serviceURL,
+      _format: format,
+      toString: toString
+    };
+
+    for (var i in queryParams) {
+      if (queryParams.hasOwnProperty(i)) {
+        var item = queryParams[i];
+        proto[i] = odataQueryFuncs[i];
+      }
+    }
+
+    // Generate a new service object
+    var F = function () {};
+    F.prototype = proto;
+    F.prototype.setFormat = function (fmt) {
+      proto._format = fmt;
+    };
+    F.prototype.setUrl = function (url) {
+      proto._root = url;
+    };
+    // Generate a new resource on the service
+    F.prototype.resource = function (url) {
+      var R = function () {
+        this._resource = [];
+        this._params = {};
+        this._default = url;
+        this.path = addResource;
+        this.id = setId;
+      };
+      R.prototype = F.prototype;
+      return new R();
+    };
+    return new F();
+  };
+
+  window.odata = {
+    string: function (str) {
+      return '\'' + str + '\'';
+    },
+    date: function (date) {
+      return odata.string(date.toISOString());
+    },
+    service: odataService
+  };
+}(window));
